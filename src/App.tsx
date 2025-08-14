ade379a800ee71517a62a9ea562d4d7647363788
Baran Şahin <sahinbaran611@gmail.com>
2025-08-14 19:30:44 +0300

    useEffect first use

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,24 @@
 
 import {View,SafeAreaView,Text,Button,FlatList,Switch} from 'react-native';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
-type Cafe = {
-  id : number;
-  name: string;
-  isFavorite : boolean;
-}
 
-const data: Cafe[] = [
-  { id: 0, name: 'cafe.exe', isFavorite: true },
-  { id: 1, name: 'KafaKafe', isFavorite: false },
-  { id: 2, name: 'BugG', isFavorite: false },
-  { id: 3, name: 'Rock n code', isFavorite: true },
-  { id: 4, name: 'do(drink)', isFavorite: false },
-  { id: 5, name: 'esc', isFavorite: false }
-];
 
 
 const App = ()=> {
-  const [cafeList, setCafeList] = useState(data);
-  const [showOnlyFavorite , setshowOnlyFavorites] = useState(false)
-
-  const onFavoriteChange =(isFavoriSelected: boolean)=>{
-    setshowOnlyFavorites(isFavoriSelected)
-    if(isFavoriSelected){
-      setCafeList(cafeList.filter(cafe => cafe.isFavorite))
-    }
-    else {
-      setCafeList(data)
-    }
-  } 
+   const [number , setNumber] = useState(0);
+   
+    
+   useEffect(()=>console.log("number updated") ,[number])
 
 
   return (
     <SafeAreaView>
-        <Switch value={showOnlyFavorite} onValueChange={onFavoriteChange}/>
-        <FlatList
-        keyExtractor={(item:Cafe)=>item.id.toString()}
-        data={cafeList}
-        renderItem={({item}:{item:Cafe})=> <Text style={{fontSize:25}}>{item.name}</Text>}
+      <Text>{number}</Text>
+      <Button title='up' onPress={()=>setNumber(number+1)}/>
         
-        />
+      
     </SafeAreaView>
   );
 
